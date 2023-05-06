c5d76d0de1907f1119c6c38a3066acf054b6708a
RuoningJin <ronjin2013@hotmail.com>
2023-05-06 00:06:16 -0400

    implement hash to password validation

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,5 +9,8 @@ module.exports = {
       host: process.env.HOST || 'localhost',
       storage: './vue-test-project.sqlite'
     }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
   }
 }
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,8 +1,41 @@
-module.exports = (sequelize, DataTypes) =>
-  sequelize.define('User', {
+const Promise = require('bluebird');
+const bcrypt = Promise.promisifyAll(require('bcryptjs'));
+
+function hashPassword(user, options) {
+  const SALT_FACTOR = 8;
+
+  if (!user.changed('password')) {
+    return;
+  }
+
+  return bcrypt
+    .genSaltAsync(SALT_FACTOR)
+    .then(salt => bcrypt.hash(user.password, salt, null))
+    .then(hash => {
+      console.log(hash);
+      user.setDataValue('password', hash)
+    })
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     email: {
       type: DataTypes.STRING,
       unique: true
     },
     password: DataTypes.STRING
+  }, {
+    hooks: {
+      beforeCreate: hashPassword,
+      beforeUpdate: hashPassword,
+      beforeSave: hashPassword
+    }
   })
+
+  User.prototype.comparePassword = async function (password) {
+    console.log(password, this.password, await bcrypt.compare(password, this.password));
+    return await bcrypt.compare(password, this.password)
+  }
+
+  return User;
+}
