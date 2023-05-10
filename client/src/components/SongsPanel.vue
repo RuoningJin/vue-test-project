095900eaf64577988700ce480ce9d8234ab83f6a
RuoningJin <ronjin2013@hotmail.com>
2023-05-09 22:24:05 -0400

    implement songs database, backend

--- client/src/components/SongsPanel.vue
+++ client/src/components/SongsPanel.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { onMounted } from 'vue';
 import PanelComponent from './PanelComponent.vue';
+import SongsService from '../services/SongsService';
+let songs = null;
+
+onMounted(() => {
+  songs = SongsService.index();
+})
 </script>
 
 <template>
   <PanelComponent title="Songs">
+    <li v-for="song in songs" :key="song.title">
+      {{ song.title }} -
+      {{ song.artist }} -
+      {{ song.album }}
+    </li>
   </PanelComponent>
 </template>
 
